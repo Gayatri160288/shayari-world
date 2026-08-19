2f6dd268753adbc550207192c100b8a4b56f2211
Gayatri Ingale <gingale@rencomgroup.com>
2026-08-19 16:19:14 +0530

    Connect frontend to production API

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/auth";
+const API_URL = "https://shayari-world-production.up.railway.app/auth";
 
 export const loginAdmin = async (email, password) => {
   const response = await axios.post(`${API_URL}/login`, {
